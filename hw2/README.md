be5b95ad6ca38cd0f0455cf0b69a31d447d98d6e
bosilca <bosilca@users.noreply.github.com>
2017-09-22 22:54:20 -0400

    Hw2 - MPI (#22)
    * Add a skeleton for the second homework.
    
    Signed-off-by: George Bosilca <bosilca@icl.utk.edu>
    
    * Add MPI support to the infrastructure.
    
    Signed-off-by: George Bosilca <bosilca@icl.utk.edu>
    
    * Hide the executable.
    This raise the hope that students will not push their
    executable into the repo and conflict with the automatic
    script pull operation.
    
    Signed-off-by: George Bosilca <bosilca@icl.utk.edu>

---
+++ hw2/README.md
@@ -0,0 +1,30 @@
+Steps to add your algorithm into the provided skeleton. The relax_template.c is supposed to provide an example on how you can easily implement multiple relaxation algorithms using the same interface.
+
+All the following steps assumes you have the most recent copy of the homework and that your shell is in the directory hw2.
+
+### Adding a new algorithm
+
+- check the defines in jacobi.h to find the one corresponding to the homework. For the MPI homework, the identifier is RELAXATION\_JACOBI_MPI. This is important as it will allow you to correctly add your algorithm.
+- make a copy of relax\_template.c as relax\_bstudent.c (or any other name you prefer), and add it as a dependency to the libhw2.a line in the Makefile.
+- in relax\_bstudent.c replace all ```template``` by ```mpi_jacobi``` (or another name of your liking).
+- at the bottom of the file change the variable \_relaxation_mpi_jacobi (because of the name change in the previous step) to have the field ```.type``` set to RELAXATION\_JACOBI_MPI.
+
+From now on if you modify the .dat examples and replace the line ```alg 0``` by ```alg 3``` (which should be the value associated in the jacobi.h to RELAXATION_JACOBI_MPI) your algorithm will be automatically called by the hw.c.
+
+### Reacting to the configuration file
+
+The elements in the configuration file (the .dat files) are fixed. The tester I provide will not understand anything that is not already there, and will therefore not be able to supply them to your algorithm. If you think there is a lack of support for some awesome feature, propose a patch via a pull request and we'll see.
+
+In any case the configuration file is loaded by the ```read_input_file``` function into a well defined structure, ```hw_params_t```. This includes the problem size you are executing as well as the number of threads that will be used (in the ```num_threads```). Your algorithm is expected to take this number in to account and start the correct number of threads and put them all to work in the resolution of the Jacobi relaxation.
+
+### Initialization/execution/finalization
+
+Preparing the environment for MPI communications should be done only once in the initialization. This will basically identify the process placement in the grid of processes, set the boundaries of the data is will handle, and everything else you might consider necessary for the safe setup of your distributed relaxation algorithm. The ```_fini``` function should do all the opposite in order to leave the software infrastructure in a sane and clean state. Unlike in the pthread example, in a distributed environment all processes will call all the functions (including ```_init```, ```_fini```, ```_apply``` and ```_coarsen```). The ```_apply``` function will the called by all processes together and they will each compute the local relaxation and the residual. A global residual will be computed by all processes using a reduction operation, and this residual will be returned by each process ```_apply```. Similarly to the previous homework, the ```_apply``` function executes a single iteration of the Jacobi relaxation.
+
+### Exposing data
+
+Last but not least the ```_get_data``` is a critical function. It is supposed to return the pointer to the local matrix that contains the most up-to-date version of the local data, i.e. the data as updated by the last iteration. This function is what my tester is using to extract the data from you algorithm and to validate it against a known set of data. It is assumed that this pointer points to the entire local data, including the ghost region around the perimeter of the local matrix tile.
+
+### Avoiding conflicts
+
+If you want to simplify your life and prevent your code from generating conflicts with my version, you should avoid as much as possible directly altering any of the provided files (that's why I suggested to make a copy of the template instead of modifying it directly). This way you will be able to easily pull any update on the COSC462 repository and integrate it seamlessly into your copy.
---
+++ hw2/hw.c
@@ -0,0 +1,247 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+
+#include "header.h"
+#include "jacobi.h"
+
+hw_params_t cosc462_hw_params = {0,};
+
+#define BUFSIZE 1024
+#define EPSILON 0.000005
+
+int read_input_file( hw_params_t* params )
+{
+    FILE *infile;
+    char buf[BUFSIZE], *cmd;
+    int i, n, ret = 0;
+
+    params->max_iterations = -1;  /* uncountable */
+    params->num_sources = 0;  /* default values */
+    params->num_threads = 1;
+    params->vis_step = 10000000;  /* big enough to never generate the graphical heat map */
+    params->alg_type = 0;  /* pick a sensible default */
+    params->num_proc_p = 1;  /* this process grid will always work */
+
+    if( NULL == params->input )
+        return -1;
+
+    infile = fopen(params->input, "r");
+    if( NULL == infile ) {
+        return -1;
+    }
+    while( NULL != fgets(buf, BUFSIZE, infile) ) {
+        if( '#' == buf[0] )
+            continue;  /* leave the comments out */
+        /* skip all spaces */
+        cmd = buf;
+        while(isspace(*cmd)) cmd++;
+
+        if( 0 == strncmp(cmd, "iter", 4) ) {
+            sscanf(cmd + 5, "%u", &params->max_iterations);
+            continue;
+        }
+        if( 0 == strncmp(cmd, "alg", 3) ) {
+            sscanf(cmd + 4, "%u", &params->alg_type);
+            continue;
+        }
+        if( 0 == strncmp(cmd, "resolution", 10) ) {
+            sscanf(cmd + 11, "%u", &params->resolution);
+            continue;
+        }
+        if( 0 == strncmp(cmd, "threads", 7) ) {
+            sscanf(cmd + 8, "%u", &params->num_threads);
+            continue;
+        }
+        if( 0 == strncmp(cmd, "vis_res", 7) ) {
+            sscanf(cmd + 8, "%u", &params->vis_res);
+            continue;
+        }
+        if( 0 == strncmp(cmd, "vis_step", 8) ) {
+            sscanf(cmd + 9, "%d", &params->vis_step);
+            continue;
+        }
+        if( 0 == strncmp(cmd, "numsrc", 6) ) {
+            sscanf(cmd + 7, "%u", &params->num_sources);
+            continue;
+        }
+        if( 0 == strncmp(cmd, "num_proc_p", 10) ) {
+            sscanf(cmd + 11, "%u", &params->num_sources);
+            continue;
+        }
+        params->heat_sources = (heat_source_t*)malloc(params->num_sources * sizeof(heat_source_t));
+        for( i = 0; i < params->num_sources; i++ ) {
+            n = sscanf(buf, "%f %f %lf %lf",
+                       &(params->heat_sources[i].x), &(params->heat_sources[i].y),
+                       &(params->heat_sources[i].range), &(params->heat_sources[i].temp));
+            if( 4 != n ) {
+                fprintf(stderr, "Input file %s malformed (pb with the heat sources)\n",
+                        params->input);
+                ret = -1;
+                goto complete_and_return;
+            }
+            /* Bail out if we are short in heat sources */
+            if( (i != (params->num_sources-1)) && (NULL == fgets(buf, BUFSIZE, infile)) ) {
+                fprintf(stderr, "Missing heat sources (%d != %d). Update the count\n",
+                        params->num_sources, i+1);
+                params->num_sources = i + 1;
+                break;
+            }
+        }
+        break;
+    }
+    params->vis_data = NULL;
+    if( COSC462_HW_FLAG_GENERATE_HEAT_IMAGE & params->flags ) {
+        params->vis_res += 2;  /* for the borders */
+        params->vis_data = (double*)malloc(params->vis_res * params->vis_res * sizeof(double));
+    }
+
+ complete_and_return:
+    fclose(infile);
+    return ret;
+}
+
+void print_params(hw_params_t* param)
+{
+    int size;
+    MPI_Comm_size(param->comm, &size);
+    printf("# Participants %d (P = %d, Q = %d)\n",
+           size, param->num_proc_p, size / param->num_proc_p);
+    printf("# Max iterations ");
+    if( 0 == param->max_iterations )
+        printf("unrestricted\n");
+    else
+        printf("%u\n", param->max_iterations);
+    printf("# Resolution %u\n",  param->resolution);
+    printf("# Visual Resolution %u\n", param->vis_res);
+    printf("# Input file %s\n", param->input);
+    printf("# Num heat sources %u\n", param->num_sources);
+    if( COSC462_HW_FLAG_VERBOSE & param->flags ) {
+        for( int i = 0; i < param->num_sources; i++ ) {
+            printf("# Heat src %d: (%f, %f) %lf %lf\n", i,
+                   param->heat_sources[i].x, param->heat_sources[i].y,
+                   param->heat_sources[i].range, param->heat_sources[i].temp);
+        }
+    }
+}
+
+static struct option long_opts[] = {
+    {"verbose", no_argument, NULL, 'v'},
+    {"time", no_argument, NULL, 't'},
+    {"image", required_argument, 0, 'i'},
+    {0, 0, 0, 0}
+};
+
+int main(int argc, char* argv[] )
+{
+    MPI_Comm jcomm;
+    int opt_idx = 0;
+    int c, csize, crank;
+
+    while( -1 != (c = getopt_long(argc, argv, "vti:", long_opts, &opt_idx)) ) {
+        switch (c) {
+        case 'v':
+            if( COSC462_HW_FLAG_VERBOSE & cosc462_hw_params.flags )
+                cosc462_hw_params.flags |= COSC462_HW_FLAG_VERY_VERBOSE;
+            else
+                cosc462_hw_params.flags |= COSC462_HW_FLAG_VERBOSE;
+            break;
+        case 't':
+            cosc462_hw_params.flags |= COSC462_HW_FLAG_TIME_ALG;
+            break;
+        case 'i':
+            cosc462_hw_params.flags |= COSC462_HW_FLAG_GENERATE_HEAT_IMAGE;
+            cosc462_hw_params.vis_output = strdup(optarg);
+            break;
+        case '?':
+            /* An error was already printed out */
+            printf("something unknown (%s)\n", (NULL == optarg ? "NULL" : optarg));
+            break;
+        }
+    }
+    /* Do we have an input filename ? */
+    if( (optind < 0) || (optind >= argc) ) {
+        fprintf(stderr, "Missing input file\n");
+        return -1;
+    }
+    cosc462_hw_params.input = strdup(argv[optind]);
+    read_input_file(&cosc462_hw_params);
+
+    /**
+     * Initialize MPI
+     */
+    MPI_Init(NULL, NULL);
+    MPI_Comm_dup( MPI_COMM_WORLD, &jcomm);
+    MPI_Comm_size( jcomm, &csize);
+    MPI_Comm_rank( jcomm, &crank);
+
+    cosc462_hw_params.comm = jcomm;
+    if( 0 == crank )  /* only the root print the banner */
+        print_params(&cosc462_hw_params);
+
+    relaxation_params_t* rp = relaxation_init(&cosc462_hw_params);
+    if( NULL == rp ) {
+        fprintf(stderr, "Error initializing the relaxation. Bail out!\n");
+        return -1;
+    }
+    if( COSC462_HW_FLAG_GENERATE_HEAT_IMAGE & cosc462_hw_params.flags ) {
+        relaxation_coarsen(rp, cosc462_hw_params.vis_data, cosc462_hw_params.vis_res, cosc462_hw_params.vis_res);
+        dump_gray_image(cosc462_hw_params.vis_output, 0,
+                        cosc462_hw_params.vis_data, cosc462_hw_params.vis_res, cosc462_hw_params.vis_res);
+    }
+
+    int iter = 0;
+    double residual, start, elapsed, ignore = 0.0;
+
+    start = wtime();
+    while(1) {
+        /* If we have to generate the images, let's not count this time towward
+         * the relaxation time.
+         */
+        if( 0 == (iter % cosc462_hw_params.vis_step) ) {
+            double s = wtime();
+            if( COSC462_HW_FLAG_GENERATE_HEAT_IMAGE & cosc462_hw_params.flags ) {
+                relaxation_coarsen(rp, cosc462_hw_params.vis_data, cosc462_hw_params.vis_res, cosc462_hw_params.vis_res);
+                /*print_matrix(cosc462_hw_params.vis_data, cosc462_hw_params.vis_res, cosc462_hw_params.vis_res);*/
+                dump_gray_image(cosc462_hw_params.vis_output, iter,
+                                cosc462_hw_params.vis_data, cosc462_hw_params.vis_res, cosc462_hw_params.vis_res);
+            }
+            if( COSC462_HW_FLAG_VERY_VERBOSE & cosc462_hw_params.flags ) {
+                relaxation_print(rp, stdout);
+            }
+            ignore += (wtime() - s);
+        }
+
+        residual = relaxation_apply(rp);
+        /* print info on the residual */
+        if( (COSC462_HW_FLAG_VERBOSE & cosc462_hw_params.flags) && (0 == (iter % cosc462_hw_params.vis_step)) ) {
+            fprintf(stderr, "Residual %lf\n", residual);
+        }
+        if( residual < EPSILON ) {  /* good enough */
+            if( COSC462_HW_FLAG_VERY_VERBOSE & cosc462_hw_params.flags ) {
+                fprintf(stderr, "Complete after %d iterations with a residual %lf < %lf\n",
+                        iter, residual, EPSILON );
+            }
+            break;
+        }
+        if( (cosc462_hw_params.max_iterations > 0) && (iter >= cosc462_hw_params.max_iterations) ) {
+            break;
+        }
+        iter++;
+    }
+    elapsed = wtime() - start - ignore;
+    if( COSC462_HW_FLAG_TIME_ALG & cosc462_hw_params.flags ) {
+        printf("Completion in %ld microseconds\n", (unsigned long int)elapsed);
+    }
+    relaxation_fini(&rp);
+
+    /**
+     * Turn off MPI.
+     */
+    MPI_Finalize();
+
+    return 0;  /* done */
+}
