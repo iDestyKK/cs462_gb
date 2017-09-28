59d46b9cbdad1c03931b9bbbaed37ed1ebe80b42
Clara Nguyen <idesty@derpg.xyz>
2017-09-28 08:44:06 -0400

    Made basis of HW

---
+++ hw2/relax_threaded.c
@@ -0,0 +1,100 @@
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "jacobi.h"
+#include "header.h"
+
+/**
+ * This is a template for an empty relaxation that can be used as an
+ * exmaple for the homework.
+ */
+struct relaxation_pthread_hidden_params_s {
+    struct relaxation_params_s super;
+    double* data;
+    uint32_t idx;
+};
+const struct relaxation_function_class_s _relaxation_pthread;
+
+static struct relaxation_params_s*
+template_relaxation_init(struct hw_params_s* hw_params)
+{
+    struct relaxation_pthread_hidden_params_s* rp;
+    uint32_t np = hw_params->resolution + 2;
+
+    rp = (struct relaxation_pthread_hidden_params_s*)malloc(sizeof(struct relaxation_pthread_hidden_params_s));
+    if( NULL == rp ) {
+        fprintf(stderr, "Cannot allocate memory for the relaxation structure\n");
+        return NULL;
+    }
+    
+    rp->super.sizex = np;
+    rp->super.sizey = np;
+    rp->super.rel_class = &_relaxation_pthread;
+
+    rp->idx = 0;
+    rp->data = calloc(np*np, sizeof(double));
+    if( NULL == rp->data ) {
+        fprintf(stderr, "Cannot allocate the memory for the matrices\n");
+        goto fail_and_return;
+    }
+
+    return (struct relaxation_params_s*)rp;
+ fail_and_return:
+    if( NULL != rp->data ) free(rp->data);
+    free(rp);
+    return NULL;
+}
+
+static int template_relaxation_fini(relaxation_params_t** prp)
+{
+    struct relaxation_pthread_hidden_params_s* rp = (struct relaxation_pthread_hidden_params_s*)*prp;
+    if( NULL != rp->data ) free(rp->data);
+    free(rp);
+    *prp = NULL;
+    return 0;
+}
+
+static int template_relaxation_coarsen(relaxation_params_t* grp, double* dst, uint32_t dstx, uint32_t dsty)
+{
+    struct relaxation_pthread_hidden_params_s* rp = (struct relaxation_pthread_hidden_params_s*)grp;
+    return coarsen(rp->data, rp->super.sizex, rp->super.sizey,
+                   dst, dstx, dsty);
+}
+
+static int template_relaxation_print(relaxation_params_t* grp, FILE* fp)
+{
+    struct relaxation_pthread_hidden_params_s* rp = (struct relaxation_pthread_hidden_params_s*)grp;
+    fprintf( fp, "\n# Iteration %d\n", rp->idx);
+    print_matrix(fp, rp->data, rp->super.sizex, rp->super.sizey);
+    fprintf( fp, "\n\n");
+    return 0;
+}
+
+/**
+ * One step of a simple Template relaxation.
+ */
+static double template_relaxation_apply(relaxation_params_t* grp)
+{
+    struct relaxation_pthread_hidden_params_s* rp = (struct relaxation_pthread_hidden_params_s*)grp;
+    fprintf(stdout, "This is only a template relaxation class. No computations are done!\n");
+    rp->idx++;
+    return 0.0;
+}
+
+static double* template_relaxation_get_data(relaxation_params_t* grp)
+{
+    return ((struct relaxation_pthread_hidden_params_s*)grp)->data;
+}
+
+
+const struct relaxation_function_class_s _relaxation_pthread =
+    {
+        .type = RELAXATION_JACOBI_PTHREADS,
+        ._init = template_relaxation_init,
+        ._fini = template_relaxation_fini,
+        ._coarsen = template_relaxation_coarsen,
+        ._print = template_relaxation_print,
+        ._relaxation = template_relaxation_apply,
+        ._get_data = template_relaxation_get_data,
+    };
--- hw2/relaxation.c
+++ hw2/relaxation.c
@@ -7,11 +7,11 @@
 
 extern const struct relaxation_function_class_s _relaxation_jacobi;
 extern const struct relaxation_function_class_s _relaxation_template;
-extern const struct relaxation_function_class_s _relaxation_pthread_jacobi;
+extern const struct relaxation_function_class_s _relaxation_pthread;
 extern const struct relaxation_function_class_s _relaxation_mpi;
 
 const struct relaxation_function_class_s * const _relaxation_classes[] =
-    {&_relaxation_jacobi, &_relaxation_template, NULL, &_relaxation_mpi};
+    {&_relaxation_jacobi, &_relaxation_template, &_relaxation_pthread, &_relaxation_mpi, NULL};
 
 relaxation_params_t* relaxation_init(struct hw_params_s* hw_params)
 {
@@ -21,8 +21,7 @@ relaxation_params_t* relaxation_init(struct hw_params_s* hw_params)
         if( _relaxation_classes[i]->type != hw_params->alg_type )
             continue;
         if( NULL == (rp =  _relaxation_classes[i]->_init(hw_params)) ) {
-			continue;
-            //return NULL;
+            return NULL;
         }
     }
     return rp;
